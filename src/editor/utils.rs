b68ebd0f0b89b1134d2a7f776871774270308359
joe <hello@murl.digital>
2024-04-23 12:42:14 -0600

    update cozy_ui

--- src/editor/utils.rs
+++ src/editor/utils.rs
@@ -1,3 +1,4 @@
+use cozy_ui::util::get_set::Operation;
 use nih_plug::{context::gui::ParamSetter, params::Param};
 
 pub fn begin_set<'a, P>(param: &'a P, setter: &'a ParamSetter<'a>) -> impl Fn() + 'a
@@ -21,12 +22,12 @@ where
 pub fn get_set_normalized<'a, P>(
     param: &'a P,
     setter: &'a ParamSetter<'a>,
-) -> impl FnMut(Option<f32>) -> f32 + 'a
+) -> impl FnMut(Operation<f32>) -> f32 + 'a
 where
     P: Param,
 {
     |value| {
-        if let Some(value) = value {
+        if let Operation::Set(value) = value {
             setter.set_parameter_normalized(param, value);
             return value;
         }
@@ -38,13 +39,13 @@ where
 pub fn get_set<'a, P>(
     param: &'a P,
     setter: &'a ParamSetter<'a>,
-) -> impl FnMut(Option<P::Plain>) -> P::Plain + 'a
+) -> impl FnMut(Operation<P::Plain>) -> P::Plain + 'a
 where
     P: Param,
     P::Plain: Copy,
 {
     |value| {
-        if let Some(value) = value {
+        if let Operation::Set(value) = value {
             setter.set_parameter(param, value);
             return value;
         }
